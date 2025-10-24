b2c5e507b2b4f80911298fc2d5603e59a76c4316
bhushanrahatecodex <bhushanrahate866@gmail.com>
2025-10-24 09:16:26 +0530

    Add files via upload

---
+++ fashion.js
@@ -0,0 +1,39 @@
+var one = document.querySelector(".one");
+one.addEventListener('click', () => {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+});
+
+var two = document.querySelector(".two");
+two.addEventListener('click', () => {
+    document.body.style.backgroundColor = "plum";
+    document.body.style.color = "black";
+});
+
+var three = document.querySelector(".three");
+three.addEventListener('click', () => {
+    document.body.style.backgroundColor = "skyblue";
+    document.body.style.color = "black";
+});
+
+var four = document.querySelector(".four");
+four.addEventListener('click', () => {
+    document.body.style.backgroundColor = "wheat";
+    document.body.style.color = "black";
+});
+
+var five = document.querySelector(".five");
+five.addEventListener('click', () => {
+    document.body.style.backgroundColor = "lightgreen";
+    document.body.style.color = "black";
+});
+
+var six = document.querySelector(".six");
+six.addEventListener('click', () => {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+
+});
+
+
+
